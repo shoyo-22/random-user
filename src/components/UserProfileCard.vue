62f746ce9616c184df5179f239d37ad06189472e
SilverBladde <shoyo2209@gmail.com>
2024-02-15 15:59:28 +0600

    refactor: implement drag and drop on elements
    Also added a new component for short user information (avatar, full name, address)

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="d-flex bg-white my-2 p-2 shadow border rounded">
+    <img
+      :src="userInfo?.picture?.thumbnail"
+      :alt="userFullName"
+      class="rounded"
+    >
+    <div class="d-flex flex-column mx-2">
+      <p class="mb-0">
+        <strong>
+          {{ userFullName }}
+        </strong>
+      </p>
+      <p class="mb-0 text-muted">
+        {{ userAddress }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfileCard',
+  props: {
+    userInfo: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    userFullName() {
+      const userName = this.userInfo?.name;
+      return `${userName?.first} ${userName?.last}`;
+    },
+    userAddress() {
+      const userLocation = this.userInfo?.location;
+      return `${userLocation?.country}, ${userLocation?.city}`;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,122 @@
 <template>
-  <h1>Random User App</h1>
+  <div class="container">
+    <h2 class="my-2">
+      Random User App
+    </h2>
+    <div class="row">
+      <div class="col mx-2 px-2 py-3 bg-light border rounded">
+        <h6>Registered</h6>
+        <p v-if="isLoading">
+          Loading...
+        </p>
+        <draggable
+          v-else
+          class="draggable-list"
+          :list="tasks.registered"
+          group="tasks"
+        >
+          <div
+            v-for="(idea, i) in tasks.registered"
+            :key="i"
+          >
+            <user-profile-card :user-info="idea" />
+          </div>
+        </draggable>
+      </div>
+      <div class="col mx-2 px-2 py-3 bg-light border rounded">
+        <h6>Moderation</h6>
+        <draggable
+          class="draggable-list"
+          :list="tasks.moderation"
+          group="tasks"
+        >
+          <div
+            v-for="(todo, i) in tasks.moderation"
+            :key="i"
+          >
+            <user-profile-card :user-info="todo" />
+          </div>
+        </draggable>
+      </div>
+      <div class="col mx-2 px-2 py-3 bg-light border rounded">
+        <h6>Approved</h6>
+        <draggable
+          class="draggable-list"
+          :list="tasks.approved"
+          group="tasks"
+        >
+          <div
+            v-for="(task, i) in tasks.approved"
+            :key="i"
+          >
+            <user-profile-card :user-info="task" />
+          </div>
+        </draggable>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+import UserProfileCard from '@/components/UserProfileCard.vue';
+
 export default {
   name: 'HomeView',
+  components: {
+    draggable,
+    UserProfileCard,
+  },
+  data() {
+    return {
+      tasks: {
+        registered: [],
+        moderation: [],
+        approved: [],
+      },
+      isLoading: false,
+    };
+  },
+  watch: {
+    // eslint-disable-next-line func-names
+    'tasks.registered': function (newArray) {
+      if (!newArray.length) {
+        this.fetchRandomUser();
+      }
+    },
+  },
+  mounted() {
+    this.fetchRandomUser();
+  },
+  methods: {
+    async fetchRandomUser() {
+      try {
+        this.isLoading = true;
+        const response = await fetch('https://randomuser.me/api');
+        const randomUserData = await response.json();
+        this.tasks.registered = [...randomUserData.results];
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+h6 {
+  font-weight: 700;
+}
+.col {
+  height: 90vh;
+  overflow: auto;
+}
+.draggable-list {
+  min-height: 10vh;
+}
+.draggable-list > div {
+  cursor: pointer;
+}
+</style>
